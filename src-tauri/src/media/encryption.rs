2c10ba98cc6edf2404bdce53688eb10bf612284c
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2025-03-21 11:13:33 +0100

    Change method call for deprecation

--- src-tauri/src/media/encryption.rs
+++ src-tauri/src/media/encryption.rs
@@ -24,7 +24,7 @@ pub fn encrypt_file(data: &[u8], key: &[u8]) -> Result<(Vec<u8>, Vec<u8>), Media
 
     let cipher = ChaCha20Poly1305::new(Key::from_slice(key));
     let mut nonce_bytes = [0u8; 12];
-    rand::thread_rng().fill_bytes(&mut nonce_bytes);
+    rand::rng().fill_bytes(&mut nonce_bytes);
     let nonce = Nonce::from_slice(&nonce_bytes);
 
     cipher
