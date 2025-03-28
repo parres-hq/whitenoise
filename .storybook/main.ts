4606b81d11f6f0a1c7bdb3304f12cc5259b9ac7e
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2025-03-28 16:17:08 +0100

    Update some storybook config and change name of github action

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from "@storybook/sveltekit";
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
-    addons: ["@storybook/addon-essentials", "@storybook/addon-svelte-csf"],
+    addons: [
+        "@storybook/addon-themes",
+        "@storybook/addon-essentials",
+        "@storybook/addon-svelte-csf",
+    ],
     framework: {
         name: "@storybook/sveltekit",
         options: {},
