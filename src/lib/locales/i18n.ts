ebf066b1b623106a363fc2134f02acef270c5d14
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2025-04-09 11:05:09 +0200

    Add german and italian, fix a few spanish translations

--- src/lib/locales/i18n.ts
+++ src/lib/locales/i18n.ts
@@ -2,6 +2,8 @@ import { getLocaleFromNavigator, init, register } from "svelte-i18n";
 
 register("en", () => import("./en.json"));
 register("es", () => import("./es.json"));
+register("it", () => import("./it.json"));
+register("de", () => import("./de.json"));
 
 export async function initI18n() {
     return init({
