da35ca35fb3c03d62d93693fe9f8478f14757acd
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2025-07-02 15:21:15 +0200

    Export aggregator types

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub use crate::whitenoise::accounts::relays::RelayType;
 pub use crate::whitenoise::accounts::{Account, AccountSettings, OnboardingState};
 pub use crate::whitenoise::error::WhitenoiseError;
 pub use nostr_mls::groups::NostrGroupConfigData;
+pub use crate::whitenoise::message_aggregator::{ChatMessage, EmojiReaction, ReactionSummary, UserReaction};
 pub use whitenoise::{Whitenoise, WhitenoiseConfig};
 
 // Re-export nostr types with documentation
